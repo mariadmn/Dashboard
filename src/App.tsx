18241a4bab511435775d7489bed8ba85914691fa
mariadmn <mariajdmn@gmail.com>
2023-04-12 15:58:01 -0300

    Criação do projeto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Dashboard from "./Pages/Dashboard/dashboard";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const App = () => {
+    return (
+        <>
+        <GlobalStyles />
+        <Dashboard />
+        </>
+    );
+}
+
+export default App;
