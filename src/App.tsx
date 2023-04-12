96dba61f43f55192b454b596f6e30706c320ffde
mariadmn <mariajdmn@gmail.com>
2023-04-12 16:44:50 -0300

    criação de tema claro e escuro

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Layout from "./components/layout/layout";
 import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import  dark from "./styles/themes/dark";
+import light from "./styles/themes/light";
 
 const App = () => {
     return (
-        <>
-        <GlobalStyles />
-        <Layout />
-        </>
+        <ThemeProvider theme={dark}>
+            <GlobalStyles />
+            <Layout />
+        </ThemeProvider>
     );
 }
 
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,16 @@
+export default{
+    title: 'light',
+    colors: {
+        primary: '#dcdde0',
+        secondary: '#fff',
+        tertiary: '#f2f3f5',
+
+        white: '#000',
+        black: '#fff',
+        gray: '#bfbfbf',
+
+        success: '#03bb85',
+        info: '#3b5998',
+        warning: '#ff6961',
+    },
+}
